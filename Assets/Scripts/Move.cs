1857fa3fc5c19b169ee8babac8bf1345f4647274
Richard <r.banks@burnley.ac.uk>
2018-10-15 14:51:02 +0100

    StartFile

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+	private Rigidbody2D rbd;
+
+	public float MoveSpeed = 25f;
+
+	private void Awake()
+	{
+		rbd = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+
+		MoveFunc(h);
+	}
+
+	private void MoveFunc(float _h)
+	{
+		rbd.AddForce(new Vector2(_h * MoveSpeed, 0f));
+	}
+}
