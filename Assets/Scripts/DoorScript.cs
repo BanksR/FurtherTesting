7b12348a2ba533a466f6daf1fd8668ccc1e8c3d4
Richard <r.banks@burnley.ac.uk>
2018-10-15 16:07:01 +0100

    xx

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+	public int doorID;
+	public string doorName;
+	public bool isUnlocked = false;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			//check the players inventory for the key that matches the doorID
+			var playerInv = other.GetComponent<Inventory>();
+			foreach (KeyPickup key in playerInv.keyChain)
+			{
+				if (key.keyID == doorID)
+				{
+					UnlockThisDoor();
+					
+					playerInv.RemoveKey(key);
+					return;
+				}
+			}
+		}
+	}
+
+	private void UnlockThisDoor()
+	{
+		Collider2D[] cols = GetComponents<Collider2D>();
+		foreach (Collider2D c in cols)
+		{
+			c.enabled = false;
+		}
+
+		isUnlocked = true;
+	}
+}
---
+++ Assets/Scripts/KeyPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickup : MonoBehaviour
+{
+
+	public int keyID;
+	public string keyName;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			other.GetComponent<Inventory>().PickUpKey(this);
+			this.gameObject.SetActive(false);
+		}
+	}
+}
